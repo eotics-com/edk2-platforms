87e456f292c25a74b15280b019985bc4f2e9aaa8
Mario Bălănică <mariobalanica02@gmail.com>
2024-01-05 19:17:57 +0200

    Platform/RPi5: Enable RNG
    Signed-off-by: Mario Bălănică <mariobalanica02@gmail.com>

--- Silicon/Broadcom/Bcm283x/Include/IndustryStandard/Bcm2838Rng.h
+++ Silicon/Broadcom/Bcm283x/Include/IndustryStandard/Bcm2838Rng.h
@@ -13,15 +13,25 @@
 #define RNG_BASE_ADDRESS                    ((FixedPcdGet64 (PcdBcm283xRegistersAddress)) \
                                             + BCM2838_RNG_OFFSET)
 
-#define RNG_CTRL                            (RNG_BASE_ADDRESS + 0x0)
-#define RNG_STATUS                          (RNG_BASE_ADDRESS + 0x4)
-#define RNG_DATA                            (RNG_BASE_ADDRESS + 0x8)
-#define RNG_BIT_COUNT                       (RNG_BASE_ADDRESS + 0xc)
-#define RNG_BIT_COUNT_THRESHOLD             (RNG_BASE_ADDRESS + 0x10)
-#define RNG_INT_STATUS                      (RNG_BASE_ADDRESS + 0x18)
-#define RNG_INT_ENABLE                      (RNG_BASE_ADDRESS + 0x1c)
-#define RNG_FIFO_DATA                       (RNG_BASE_ADDRESS + 0x20)
-#define RNG_FIFO_COUNT                      (RNG_BASE_ADDRESS + 0x24)
+#define RNG_CTRL_OFFSET                     0x0
+#define RNG_STATUS_OFFSET                   0x4
+#define RNG_DATA_OFFSET                     0x8
+#define RNG_BIT_COUNT_OFFSET                0xc
+#define RNG_BIT_COUNT_THRESHOLD_OFFSET      0x10
+#define RNG_INT_STATUS_OFFSET               0x18
+#define RNG_INT_ENABLE_OFFSET               0x1c
+#define RNG_FIFO_DATA_OFFSET                0x20
+#define RNG_FIFO_COUNT_OFFSET               0x24
+
+#define RNG_CTRL                            (RNG_BASE_ADDRESS + RNG_CTRL_OFFSET)
+#define RNG_STATUS                          (RNG_BASE_ADDRESS + RNG_STATUS_OFFSET)
+#define RNG_DATA                            (RNG_BASE_ADDRESS + RNG_DATA_OFFSET)
+#define RNG_BIT_COUNT                       (RNG_BASE_ADDRESS + RNG_BIT_COUNT_OFFSET)
+#define RNG_BIT_COUNT_THRESHOLD             (RNG_BASE_ADDRESS + RNG_BIT_COUNT_THRESHOLD_OFFSET)
+#define RNG_INT_STATUS                      (RNG_BASE_ADDRESS + RNG_INT_STATUS_OFFSET)
+#define RNG_INT_ENABLE                      (RNG_BASE_ADDRESS + RNG_INT_ENABLE_OFFSET)
+#define RNG_FIFO_DATA                       (RNG_BASE_ADDRESS + RNG_FIFO_DATA_OFFSET)
+#define RNG_FIFO_COUNT                      (RNG_BASE_ADDRESS + RNG_FIFO_COUNT_OFFSET)
 
 #define RNG_CTRL_ENABLE_MASK                0x1fff
 #define RNG_CTRL_SAMPLE_RATE_DIVISOR_SHIFT  13      // Unmasked bits from above
